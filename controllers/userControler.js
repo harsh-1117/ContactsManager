097b02f3cadc9e6b0594affb76d30cd63c421edf
Harsh <harshag9354@gmail.com>
2025-02-19 13:05:09 +0530

    Project Added

--- controllers/userControler.js
+++ controllers/userControler.js
@@ -51,7 +51,7 @@ const loginUser = asyncHandler(async (req,res)=>{
                 id: user.id
             }   
         },
-        process.env.ACCESS_TOKEN_SECRET,
+        "HarshsProject1",
         { expiresIn: "30m" }
         );
         res.status(200).json(accessToken);
