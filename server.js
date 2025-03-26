2f8c977c94456f6a8e5965bf43ecd5455e49e3a6
Jahnavi Majumder <majumderj811@gmail.com>
2025-03-26 08:40:13 +0530

    fixed cors error

--- server.js
+++ server.js
@@ -3,12 +3,13 @@ const errorHandler = require("./middleware/errorHandler");
 const connectDB = require("./config/dbConnection");
 const passport = require("./config/passport");
 const dotenv = require("dotenv").config();
+const cors = require('cors');
 
 const app = express();
 connectDB();
 
 const port = process.env.PORT || 5000;
-
+app.use(cors());
 app.use(express.json());
 app.use(passport.initialize()); // Only initialize Passport (No sessions)
 
